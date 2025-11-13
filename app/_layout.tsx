c072b7eb7800b1b9f4db7bdc64ac7da435966f8a
Namra13INFP <namrasaleem346@gmail.com>
2025-11-13 21:50:14 +0500

    onesignal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,13 @@
-import { useEffect } from 'react';
-// Include the OneSignal package
-import { LogLevel, OneSignal } from 'react-native-onesignal';
+import { Slot } from "expo-router";
+import { useEffect } from "react";
+import { LogLevel, OneSignal } from "react-native-onesignal";
 
 export default function RootLayout() {
-  // Initialize OneSignal in useEffect to ensure it runs only once
   useEffect(() => {
-    // Enable verbose logging for debugging (remove in production)
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
-    // Initialize with your OneSignal App ID
-    OneSignal.initialize('bbbc7661-47ee-42d1-8ac0-b6cac97109cb');
-    // Use this method to prompt for push notifications.
-    // We recommend removing this method after testing and instead use In-App Messages to prompt for notification permission.
-    OneSignal.Notifications.requestPermission(false);
-  }, []); // Ensure this only runs once on app mount
+    OneSignal.initialize("bbbc7661-47ee-42d1-8ac0-b6cac97109cb");
+    OneSignal.Notifications.requestPermission(true);
+  }, []);
+
+  return <Slot />; 
 }
